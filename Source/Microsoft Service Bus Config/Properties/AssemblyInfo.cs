ef03443e681aae82a686a3ec33b5841f4fdb92eb
Paul Turner <programming.hero@gmail.com>
2013-04-14 21:46:32 +0100

    Added basic AssemblyInfo metadata
    Updated AssemblyInfo.cs for primary assembly to contain standard set of
    attributes and "magic" values to support build automation.

--- Source/Microsoft Service Bus Config/Properties/AssemblyInfo.cs
+++ Source/Microsoft Service Bus Config/Properties/AssemblyInfo.cs
@@ -1,18 +1,24 @@
 ﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Service Bus Config")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Service Bus Config")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("Microsoft Service Bus Config")]
+[assembly: AssemblyDescription("Provides configuration utilities for Microsoft Service Bus namespaces.")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("PyPup Software")]
+[assembly: AssemblyProduct("PyPup Infrastructure")]
+[assembly: AssemblyCopyright("Copyright © 2013")]
+
+[assembly: NeutralResourcesLanguageAttribute("en-GB")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -22,15 +28,16 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2fa16886-9a54-4137-adc1-f30309ad441a")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+// Assembly Version is updated manually to provide .NET compatibility between release versions.
+// The Major and Minor values are updated to keep in-line with the Assembly Informational Version.
 [assembly: AssemblyVersion("1.0.0.0")]
+
+// Assembly File Version is updated automatically by the build process.
+// The Major and Minor values are kept in-line with the Assembly Informational Version.
+// The Build and Revision values are assigned by the build process.
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Assembly Information Version is updated according to Semantic Versioning rules: http://semver.org/ 
+// The value is assigned by the build process.
+// This is the version number used when publishing a NuGet package for the assembly.
+[assembly: AssemblyInformationalVersion("1.0.0-Dev")]
